dc9f6f880f42d84f890e2671ad1ff0cbac47230f
Satyam216 <satyamjain216@gmail.com>
2025-10-11 02:48:01 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/dashboard";
 
 function App() {
   return (
